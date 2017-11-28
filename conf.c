e6427c8b380a0e614644d6cd34762bfa8906f940
Glenn Upthagrove <upthagrg@flip3.engr.oregonstate.edu>
2017-11-27 17:03:09 -0800

    added configure file and program

---
+++ conf.c
@@ -0,0 +1,13 @@
+/*******************************************************
+ * Tile: Conf.c
+ * Author Glenn Upthagrove
+ * Date: 11/27/2017
+ * Description: A program to configure the utility.
+ * In it's current state, cgargbage does not ned this.
+ * This will be kept for future use if needed, also
+ * travis ci needs this program to exist. 
+*******************************************************/
+int main(){
+	//TRIVIAL RETURN
+	return 0;
+}
